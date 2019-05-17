80eddb0a3c7a972f5020a6be193b3a49412d600f
Mitchell Henke <mitchell@mitchellhenke.com>
2019-05-16 19:29:42 -0500

    change text and add Milwaukee seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,9 @@ User.where(email: 'admin@example.com').first_or_initialize.tap do |user|
   user.save!
 end
 
-r = Random.new
-
-500.times do |i|
-  Thing.where(city_id: i).first_or_initialize.tap do |thing|
-    thing.name = "Some Drain #{i}"
-    thing.lat = r.rand(37.75..37.78)
-    thing.lng = r.rand(-122.43..-122.41)
-    thing.system_use_code = ['MS4', 'STORM', 'COMB', 'UNK'].sample
-    thing.priority = [true, false].sample
-    thing.save!
-  end
-end
+drain = Thing.new(name: "600 E Greenfield Ave, Milwaukee", lat: 42.9980238995142, lng: -87.8919006529653, system_use_code: "STORM", priority: false)
+drain.save!
+drain = Thing.new(name: "730 N Old World 3rd St, Milwaukee", lat: 43.0395624298661, lng: -87.9140865979326, system_use_code: "STORM", priority: false)
+drain.save!
+drain = Thing.new(name: "1671 N Marshall St, Milwaukee", lat: 43.0523156560191, lng: -87.901244986548, system_use_code: "STORM", priority: false)
+drain.save!
